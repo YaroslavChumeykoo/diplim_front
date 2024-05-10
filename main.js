0e5dac37b7805f4e03a4b2f34100cd4c7618c1ca
ярослав чумейко <00yasik00@gmail.com>
2024-05-11 00:01:39 +0300

    v1

--- main.js
+++ main.js
@@ -12,12 +12,23 @@ const inputEmailRegistr = document.querySelector('#registration_email');
 const inputPasswordRegistr = document.querySelector('#registration_password');
 const butRegistr = document.querySelector('.registration_button');
 const butAllTasks = document.querySelector('.tasks-all');
+const mainSection = document.querySelector('.main_section');
+const containerTasks = document.querySelector('.for-tasks');
+const containerForTasks = document.querySelector('.container-tasks');
+const typeTasks = document.querySelector('.type-tasks');
+const levelTasks = document.querySelector('.level-tasks');
+const createNewTask = document.querySelector('.create-task');
+const containerCreateTask = document.querySelector('.for-create-task');
+const butAllUsers = document.querySelector('.all-users');
+const containerAllUsers = document.querySelector('.container-for-all-users');
 
 
 
 const url = 'http://localhost:3001/'
 let token = '';
 let role = '';
+let tasks = [];
+let users = [];
 
 
 const logOut = () => {
@@ -30,6 +41,10 @@ const logOut = () => {
   nameUser.innerHTML = `You: Guest`;
   localStorage.removeItem('data');
   localStorage.removeItem('email');
+  mainSection.classList.remove('none');
+  containerTasks.classList.add('none');
+  containerCreateTask.classList.add('none');
+  containerAllUsers.classList.add('none')
 }
 
 const successLogin = (data, email) => {
@@ -41,6 +56,7 @@ const successLogin = (data, email) => {
     butLogIn.classList.add('none');
     butLogUs.classList.add('none');
     butLogOut.classList.remove('none');
+    butAllUsers.classList.remove('none');
     modalWindowLogin.classList.add('none');
     modalWindowRegistr.classList.add('none');
     roleUser.innerHTML = `Your role: ${role}`;
@@ -101,7 +117,53 @@ const registration = () => {
   }).catch((res) => alert(res))
 }
 
+const renderAllUsers = () => {
+  let renderList = '';
+  users.forEach((user) => {
+    const render = `
+    <div class="user" id="${user.id}">
+      <p>Email: ${user.email}</p>
+      <p>Role: ${user.role}</p>
+      <button onclick="updateUserRole('admin','${user.role}', '${user.id}')">Add admin role</button>
+      <button onclick="updateUserRole('user','${user.role}', '${user.id}')">Remove admin role</button>
+    </div>`
+    renderList += render;
+  });
+  containerAllUsers.innerHTML = `<h1>List All Users</h1>${renderList}`
+}
+
+const updateUserRole = (addRole, userRole, id) => {
+  if (addRole === userRole) {
+    alert(`This user is already: ${userRole}`)
+  } else {
+    fetch(`${url}users/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json'  
+      },
+      body: JSON.stringify({
+        role: addRole
+      })
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error('error');
+        return res.json();
+      })
+      .then((data) => {
+        users.forEach((user) => {
+          if (user.id === data.id) user.role = data.role
+        })
+        renderAllUsers()
+      })
+  }
+};
+
 const getAllUser = () => {
+  containerTasks.classList.add('none');
+  containerCreateTask.classList.add('none');
+  mainSection.classList.add('none')
+  containerAllUsers.classList.remove('none')
   fetch(url + 'users', {
     method: 'GET',
     headers: {
@@ -111,12 +173,111 @@ const getAllUser = () => {
     if (!res.ok) return res.json().then(err => {throw new Error(`${err.message}`)});
       return res.json();
   })
-  .then((data) => console.log(data))
+  .then((data) => {
+    users = data
+    renderAllUsers()
+  })
   .catch((res) => alert(res))
 }
 
-const getAllTasks = () => {
 
+const requestAddNewTask = () => {
+  if (containerCreateTask.childNodes[3].value === '' 
+  || containerCreateTask.childNodes[9].value === '') {
+    alert('Enter the name and text of the task');
+  } else {
+    const photo = new Blob([containerCreateTask.childNodes[11].files[0]])
+    const formData = new FormData();
+    formData.append('name', containerCreateTask.childNodes[3].value);
+    formData.append('text', containerCreateTask.childNodes[9].value);
+    formData.append('tag', containerCreateTask.childNodes[5].childNodes[1].value);
+    formData.append('level', containerCreateTask.childNodes[7].childNodes[1].value);
+    
+    // Проверка, есть ли файл
+    const fileInput = containerCreateTask.childNodes[11].files[0];
+    if (fileInput) {
+      formData.append('photo', photo);
+    }
+    containerCreateTask.childNodes[3].value = '';
+    containerCreateTask.childNodes[9].value = '';
+    containerCreateTask.childNodes[11].value = '';
+
+    fetch(`${url}tasks`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,  
+      },
+      body: formData, 
+    })
+    .then(res => {
+      if(!res.ok) throw new Error('error');
+      return res.json();
+    })
+    .then(data => {
+      tasks.push(data);
+      renderTasks()
+    })
+  }
+
+};
+
+const removeTask = (id) => {
+  fetch(`${url}tasks/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${token}`,  
+    },
+  })
+    .then((res) => {
+      if(!res.ok) throw new Error('error');
+      return res
+    })
+    .then(() => {
+      tasks = tasks.filter((item) => item.id !== id);
+    renderTasks();
+  })
+  
+}
+
+const renderTasks = () => {
+  containerCreateTask.classList.add('none');
+  mainSection.classList.add('none');
+  containerTasks.classList.remove('none');
+  containerAllUsers.classList.add('none')
+  let renderList = ``;
+ 
+  tasks.forEach((item) => {
+    if ((typeTasks.value === item.tag || 
+    typeTasks.value === 'ALL') &&
+    (levelTasks.value === item.level.toUpperCase() || levelTasks.value === 'ALL')) {
+      const render = `
+      <div class="task">
+        <div class="task-name">${item.name.toUpperCase()}</div>
+        <div class="tag">Type: ${item.tag.toUpperCase()}</div>
+        <div class="level">Level: ${item.level}</div>
+        <div class="task-text">${item.text}</div>
+        <img class="photo-task" src="${item.photo ? item.photo : 'style/task.webp'}">
+        <button onclick="removeTask(${item.id})" class="remove-task ${role === 'admin' ? '' : 'none'}">Remove Task</button>
+      </div>
+      `
+      renderList += render
+    }
+  });
+  containerForTasks.innerHTML = renderList
+
+  if(role === 'admin') {
+    const butAddTask = document.createElement('button');
+    butAddTask.classList.add('addTask');
+    butAddTask.addEventListener('click', () => {
+      containerTasks.classList.add('none');
+      containerCreateTask.classList.remove('none');
+    });
+    butAddTask.innerHTML = 'Add Task'
+    containerForTasks.prepend(butAddTask);
+  }
+}
+
+const getAllTasks = () => {
   fetch(url + 'tasks', {
     method: 'GET',
     headers: {
@@ -126,8 +287,12 @@ const getAllTasks = () => {
     if (!res.ok) return res.json().then(err => {throw new Error(`${err.message}`)});
       return res.json();
   })
-  .then((data) => console.log(data))
+  .then((data) => {
+    tasks = data
+    renderTasks()
+  })
   .catch((res) => alert(res))
+
 }
 
 const showModal = (event) => {
@@ -143,6 +308,8 @@ const hideModal = (event) => {
   }
 }
 
+levelTasks.addEventListener('change', renderTasks);
+typeTasks.addEventListener('change', renderTasks);
 butLogIn.addEventListener('click', showModal);
 butLogUs.addEventListener('click', showModal);
 modalWindowLogin.addEventListener('click', hideModal);
@@ -151,3 +318,5 @@ butLogin.addEventListener("click", login);
 butRegistr.addEventListener('click', registration);
 butLogOut.addEventListener('click', logOut);
 butAllTasks.addEventListener('click', getAllTasks);
+createNewTask.addEventListener('click', requestAddNewTask);
+butAllUsers.addEventListener('click', getAllUser)
